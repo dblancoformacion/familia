a3f688095a5e9cd3c04d4d7d47e0e42c65f3ea79
dblancoformacion <dblancoformacion@gmail.com>
2018-09-29 16:42:39 +0200

    Factorial

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ while($r=$rs->fetch_assoc()){
 $txt.='</ol>';
 // arbol
 $rs=$conn->query("CALL arbol(1);");
-$grado=0;
+$grado=1;
 $txt.='<ol>';
 while($r=$rs->fetch_assoc()){
 	if($grado<$r['grado']){
@@ -23,6 +23,22 @@ while($r=$rs->fetch_assoc()){
 	}
 	$txt.='<li>'.$r['persona'].' ('.$r['nacimiento'].')</li>';
 }
-$txt.='</ol>';
+for($i=0;$i<$grado;$i++) $txt.='</ol>';
 echo utf8_encode($txt);
+// factoriales
+echo '<br/><br/>';
+echo factorial(5);
+echo '<br/><br/>';
+echo factorial_iterativo(5);
+// funciones
+function factorial($n){
+	$r=1;
+	if($n>1) $r=$n*factorial($n-1);
+	return $r;
+}
+function factorial_iterativo($n){
+	$r=1;
+	for($i=1;$i<=$n;$i++) $r*=$i;
+	return $r;
+}
 ?>
